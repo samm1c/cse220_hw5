d0612517a610c6ac0d8ecbe3097abde293cfc5c5
sammi <158765072+samm1c@users.noreply.github.com>
2024-12-02 01:22:56 +0000

    p1 print

--- hw5.asm
+++ hw5.asm
@@ -47,16 +47,54 @@ placePieceOnBoard:
 
 piece_done:
     jr $ra
+    
 # Function: printBoard
 # Arguments: None (uses global variables)
 # Returns: void
 # Uses global variables: board (char[]), board_width (int), board_height (int)
-
 printBoard:
     # Function prologue
+    
+    # load board first b/c we need to perform operations on their addresses
+    la $t0, board_width 	# t0 -> holds width address
+    lw $t1, 0($t0)		# t1 = board_width
+    la $t0, board_height	# t0 -> holds height address
+    lw $t2, 0($t0)		# t2 = board_height
+    la $t3, board		# t3 -> holds board address
+    
+    # iterate through the board (rows first)
+    li $t4, 0			# t4 = i = 0 = row index
+outer_loop:
+    bge $t4, $t2, end_print	# i >= height -> end of loop
+    # iterate through the columns
+    li $t5, 0			# t5 = j = 0 = column index
+inner_loop:
+    bge $t5, $t1, new_row	# j >= width -> end of loop
+    
+    # calulate board[i][j]
+    mul $t6, $t4, $t1 		# t6 = i * width -> skip previous rows
+    add $t6, $t6, $t5		# t6 = (i * width) + j -> add j column index
+    add $t6, $t3, $t6		# t6 = board address + offset = address of board[i][j]
+    
+    # print the character
+    lb $a0, 0($t6)		# a0 = address of board[i][j] -> function argument for syscall to print
+    li $v0, 11			# 11 -> print in syscall
+    syscall
+    
+    addi $t5, $t5, 1		# j++
+    j inner_loop		# repeat loop
+new_row:
+    # print new line
+    li $a0, 10			# a0 = 10 = ASCII for \n -> function argument for syscall to print
+    li $v0, 11			# 11 -> print
+    syscall
+    
+    addi $t4, $t4, 1		# i++
+    j outer_loop		# repeat loop
+end_print:
 
     # Function epilogue
-    
+
     jr $ra                # Return
 
 # Function: place_tile
@@ -67,7 +105,6 @@ printBoard:
 # Returns:
 #   $v0 - 0 if successful, 1 if occupied, 2 if out of bounds
 # Uses global variables: board (char[]), board_width (int), board_height (int)
-
 place_tile:
     jr $ra
 
