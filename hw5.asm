04d2216db5b21611875b56a12b5a111d058abc5d
sammi <158765072+samm1c@users.noreply.github.com>
2024-12-02 01:50:25 +0000

    p1 done; clean-up including label names

--- hw5.asm
+++ hw5.asm
@@ -54,7 +54,6 @@ piece_done:
 # Uses global variables: board (char[]), board_width (int), board_height (int)
 printBoard:
     # Function prologue
-    
     # load board first b/c we need to perform operations on their addresses
     la $t0, board_width 	# t0 -> holds width address
     lw $t1, 0($t0)		# t1 = board_width
@@ -62,44 +61,38 @@ printBoard:
     lw $t2, 0($t0)		# t2 = board_height
     la $t3, board		# t3 -> holds board address
     
-    # iterate through the board (rows first)
+    # loop start
     li $t4, 0			# t4 = i = 0 = row index
-outer_loop:
-    bge $t4, $t2, end_print	# i >= height -> end of loop
-    # iterate through the columns
+print_outer_loop:
+    bge $t4, $t2, print_end	# i >= height -> end of loop
     li $t5, 0			# t5 = j = 0 = column index
-inner_loop:
-    bge $t5, $t1, new_row	# j >= width -> end of loop
-    
+print_inner_loop:
+    bge $t5, $t1, print_new_row	# j >= width -> end of loop
     # calulate board[i][j]
     mul $t6, $t4, $t1 		# t6 = i * width -> skip previous rows
     add $t6, $t6, $t5		# t6 = (i * width) + j -> add j column index
     add $t6, $t3, $t6		# t6 = board address + offset = address of board[i][j]
-    
     # print the number
     lb $a0, 0($t6)		# a0 = address of board[i][j] -> function argument for syscall to print
     li $v0, 1			# 11 -> print integer in syscall
     syscall
-    
     # print a space after
     li $a0, 32			# a0 = 32 = ASCII for space
     li $v0, 11			# 11 -> print char
     syscall
-    
+    # loop management
     addi $t5, $t5, 1		# j++
-    j inner_loop		# repeat loop
-new_row:
+    j print_inner_loop		# repeat loop
+print_new_row:
     # print new line
     li $a0, 10			# a0 = 10 = ASCII for \n -> function argument for syscall to print
     li $v0, 11			# 11 -> print char
     syscall
-    
+    # loop management
     addi $t4, $t4, 1		# i++
-    j outer_loop		# repeat loop
-end_print:
-
+    j print_outer_loop		# repeat loop
+print_end:
     # Function epilogue
-
     jr $ra                # Return
 
 # Function: place_tile
